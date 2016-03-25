e19ef711fda2767fb2a6f5fa3aa2f09e3caa2101
Mikko Leino <mikko@viiksipojat.fi>
2016-03-25 14:59:20 +0200

    Add Event post type

--- wp-content/themes/wonderlust/functions.php
+++ wp-content/themes/wonderlust/functions.php
@@ -150,3 +150,32 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+// Events custom post type
+function custom_event_type() {
+	$labels = array(
+    'name'               => _x( 'Events', 'post type general name' ),
+    'singular_name'      => _x( 'Event', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', 'book' ),
+    'add_new_item'       => __( 'Add New Event' ),
+    'edit_item'          => __( 'Edit Event' ),
+    'new_item'           => __( 'New Event' ),
+    'all_items'          => __( 'All Events' ),
+    'view_item'          => __( 'View Event' ),
+    'search_items'       => __( 'Search Events' ),
+    'not_found'          => __( 'No events found' ),
+    'not_found_in_trash' => __( 'No events found in the Trash' ),
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Events'
+  );
+ 	$args = array(
+    'labels'        => $labels,
+    'description'   => 'Holds our events and event specific data',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'event', $args );
+}
+add_action( 'init', 'custom_event_type' );
