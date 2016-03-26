cf40751fd3dd8219f7a3e7a09aabda9908c388a9
Mikko Leino <mikko@viiksipojat.fi>
2016-03-26 20:55:47 +0200

    Add event fields to the template

--- wp-content/themes/wonderlust/template-parts/content-event.php
+++ wp-content/themes/wonderlust/template-parts/content-event.php
@@ -11,7 +11,11 @@
 
 <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="entry-header">
+    <h2 class="date"><?php the_field('date'); ?></h2>
+    <?php the_post_thumbnail('full'); ?>
     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    <h2 class="location"><?php the_field('location'); ?></h2>
+    <h2 class="price"><?php the_field('price'); ?></h2>
   </header><!-- .entry-header -->
 
   <div class="entry-content">
