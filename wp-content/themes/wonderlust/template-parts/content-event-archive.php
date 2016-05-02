9552b7be528f2365945a30cf633d133db184ee2d
Mikko Leino <mikko@viiksipojat.fi>
2016-05-02 20:48:17 +0300

    Add URL to anchors :)

--- wp-content/themes/wonderlust/template-parts/content-event-archive.php
+++ wp-content/themes/wonderlust/template-parts/content-event-archive.php
@@ -13,7 +13,7 @@
   <header class="entry-header">
     <h2 class="date"><?php the_field('date'); ?></h2>
     <?php the_post_thumbnail('full'); ?>
-    <a href="" class="entry-title"><?php the_title(); ?></a>
+    <a href="<?php the_permalink(); ?>" class="entry-title"><?php the_title(); ?></a>
     <h2 class="location"><?php the_field('location'); ?></h2>
     <h2 class="price"><?php the_field('price'); ?></h2>
   </header><!-- .entry-header -->
