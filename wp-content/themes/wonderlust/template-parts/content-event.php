1c93013d2d85de477ba8f8839b2d845c009cc1c4
Mikko Leino <mikko@viiksipojat.fi>
2016-05-02 20:55:30 +0300

    Reorder info, add buy a ticket link

--- wp-content/themes/wonderlust/template-parts/content-event.php
+++ wp-content/themes/wonderlust/template-parts/content-event.php
@@ -11,11 +11,23 @@
 
 <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="entry-header">
-    <h2 class="date"><?php the_field('date'); ?></h2>
-    <?php the_post_thumbnail('full'); ?>
     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    <h2 class="date"><?php the_field('date'); ?></h2>
     <h2 class="location"><?php the_field('location'); ?></h2>
-    <h2 class="price"><?php the_field('price'); ?></h2>
+    <h2 class="price"><?php
+      if (get_field('purchase_url')):
+        echo '<a href="' . get_field('purchase_url') . '">' . get_field('price') . ' ';
+        if (get_locale() == 'fi_FI'):
+          echo 'OSTA LIPPU';
+        else:
+          echo 'BUY A TICKET';
+        endif;
+        echo '</a>';
+      else:
+       the_field('price');
+      endif;
+    ?></h2>
+    <?php the_post_thumbnail('full'); ?>
   </header><!-- .entry-header -->
 
   <div class="entry-content">
