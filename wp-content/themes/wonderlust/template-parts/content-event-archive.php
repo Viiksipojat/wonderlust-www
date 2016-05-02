81292fced9fa0acdea862a7c69133b6172712b1b
Mikko Leino <mikko@viiksipojat.fi>
2016-05-02 21:26:29 +0300

    Use image made for programme if one exists

--- wp-content/themes/wonderlust/template-parts/content-event-archive.php
+++ wp-content/themes/wonderlust/template-parts/content-event-archive.php
@@ -12,7 +12,14 @@
 <article id="event-<?php the_ID(); ?>" <?php post_class('event-archive'); ?>>
   <header class="entry-header">
     <h2 class="date"><?php the_field('date'); ?></h2>
-    <?php the_post_thumbnail('full'); ?>
+    <?php
+      if (get_field('programme_image')):
+        // <img width="380" height="606" src="http://vu2111.evelyn.1984.is/wp-content/uploads/2016/05/Untitled.png" class="attachment-full size-full wp-post-image" alt="Image courtesy of Nehra Stella" srcset="http://vu2111.evelyn.1984.is/wp-content/uploads/2016/05/Untitled-188x300.png 188w, http://vu2111.evelyn.1984.is/wp-content/uploads/2016/05/Untitled.png 380w" sizes="(max-width: 380px) 100vw, 380px">
+        echo '<img width="380" src="' . get_field('programme_image') . '">';
+      else:
+        the_post_thumbnail('full');
+      endif;
+    ?>
     <a href="<?php the_permalink(); ?>" class="entry-title"><?php the_title(); ?></a>
     <h2 class="location"><?php the_field('location'); ?></h2>
     <h2 class="price"><?php
