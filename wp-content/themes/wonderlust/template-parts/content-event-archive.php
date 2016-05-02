f83fae2d709f875d99cb8b98e39aef29760058e7
Mikko Leino <mikko@viiksipojat.fi>
2016-05-02 21:18:47 +0300

    Add buy a ticket links to the programme

--- wp-content/themes/wonderlust/template-parts/content-event-archive.php
+++ wp-content/themes/wonderlust/template-parts/content-event-archive.php
@@ -15,6 +15,18 @@
     <?php the_post_thumbnail('full'); ?>
     <a href="<?php the_permalink(); ?>" class="entry-title"><?php the_title(); ?></a>
     <h2 class="location"><?php the_field('location'); ?></h2>
-    <h2 class="price"><?php the_field('price'); ?></h2>
+    <h2 class="price"><?php
+      if (get_field('purchase_url')):
+        echo '<a href="' . get_field('purchase_url') . '">' . get_field('price') . ' ';
+        if (get_locale() == 'fi_FI'):
+          echo '/ OSTA LIPPU';
+        else:
+          echo '/ BUY A TICKET';
+        endif;
+        echo '</a>';
+      else:
+       the_field('price');
+      endif;
+    ?></h2>
   </header><!-- .entry-header -->
 </article><!-- #post-## -->
